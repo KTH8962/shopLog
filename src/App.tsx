481751362d17d42a37151063cac7358a4719f8f6
KTH8962 <gaya8962@gmail.com>
2025-02-12 22:01:23 +0900

    기본 셋팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { BrowserRouter as Router, Routes } from "react-router-dom"
-import styles from "./App.module.scss"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+//import styles from "./App.module.scss"
 import Header from "@components/header/Header"
+import MainPage from "@pages/MainPage"
+import MyPage from "@pages/MyPage"
+import Login from "@pages/Login"
+import Join from "@pages/Join"
 
 function App() {
   return (
-    <div className={styles.div}>
-      <Router>
-        <Header />
-        <Routes></Routes>
-      </Router>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" index element={<MainPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+      </Routes>
+    </Router>
   )
 }
 
