79ce4166adac0fb75af6e8799e53f9b4a5eb6e73
KTH8962 <gaya8962@gmail.com>
2025-03-03 10:18:59 +0900

    회원가입 정리

--- src/pages/join/Join.tsx
+++ src/pages/join/Join.tsx
@@ -39,7 +39,7 @@ function Join() {
             <div className={styles.tit_box}>이름</div>
             <div className={styles.bot_box}>
               <div className={styles.ip_box}>
-                <input type="text" />
+                <input type="text" placeholder="이름을 입력해주세요" />
               </div>
             </div>
           </div>
